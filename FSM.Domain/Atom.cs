721abd1c41259b83952b3563cb6d4a3cf06e3ed7
190680 <190680@NTB-00727>
2017-04-17 16:45:30 -0300

    Add project files.

---
+++ FSM.Domain/Atom.cs
@@ -0,0 +1,20 @@
+﻿namespace FSM.Domain
+{
+    public class Atom
+    {
+        public int Serial { get; set; }
+        public string Name { get; set; }
+        public char AlternateLocationIndicator { get; set; }
+        public string ResidueName { get; set; }
+        public char ChainID { get; set; }
+        public int ResidueSequenceNumber { get; set; }
+        public int CodeForInsertionOfResidues { get; set; }
+        public double X { get; set; }
+        public double Y { get; set; }
+        public double Z { get; set; }
+        public double Occupancy { get; set; }
+        public double TemperatureFactor { get; set; }
+        public string Elemente { get; set; }
+        public string Charge { get; set; }
+    }
+}
---
+++ FSM.Repository/PDBRespository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FSM.Repository
+{
+    public class PDBRespository
+    {
+        public void LoadPDBData(string path)
+        {
+            var directory = Path.GetDirectoryName(path);
+        }
+    }
+}
